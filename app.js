8f431bbd2d06fae80ee5624046b8780aef95606e
haapamakituomas <115136792+haapamakituomas@users.noreply.github.com>
2023-08-28 10:07:27 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  // Initialize votes with an array of zeros
+  const initialVotes = new Array(anecdotes.length).fill(0)
+  const [votes, setVotes] = useState(initialVotes)
+  const [selected, setSelected] = useState(0)
+
+  const randomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const voteAnecdote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  // Find the index of the most voted anecdote
+  const mostVotedIndex = votes.indexOf(Math.max(...votes))
+
+  return (
+    <div>
+      <h2>Anecdote of the Day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>Votes: {votes[selected]}</p>
+      <button onClick={voteAnecdote}>Vote</button>
+      <button onClick={randomAnecdote}>Next Anecdote</button>
+
+      <h2>Most Voted Anecdote</h2>
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>Votes: {votes[mostVotedIndex]}</p>
+    </div>
+  )
+}
+
+export default App
